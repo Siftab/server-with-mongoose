3f990e975e9b69085e43242f778e9938134a8e0c
Siftab <siiffuuu@gmail.com>
2024-06-08 16:29:56 +0600

    model create

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -1,13 +1,29 @@
 import { Schema, model, connect } from 'mongoose';
-import { Student } from './student.interface';
+import { Guardian, LocalGuardian, Student, Student, UserName } from './student.interface';
+const UserNameSchema = new Schema<UserName>({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true }
+
+})
+const GuardianSchema = new Schema<Guardian>({
+    fatherName: { type: String },
+    fatherOcupation: { type: String },
+    fatherContactNo: { type: String },
+    motherName: { type: String },
+    motherOcupation: { type: String },
+    motherContactNo: { type: String },
+})
+const localGuardianShcema = new Schema<LocalGuardian>({
+    name: { type: String },
+    contactNo: { type: String },
+    ocupation: { type: String },
+    adress: { type: String }
+
+})
 
 const StudentSchema = new Schema<Student>({
     id: { type: String },
-    name: {
-        firstName: { type: String, required: true },
-        lastName: { type: String, required: true }
-
-    }
+    name: UserNameSchema
     , gender: ["female", "male"],
     email: { type: String },
     conactNo: { type: String },
@@ -18,23 +34,12 @@ const StudentSchema = new Schema<Student>({
     presentAddress: { type: String },
     permanentAddress: { type: String }
     ,
-    guardian: {
-        fatherName: { type: String },
-        fatherOcupation: { type: String },
-        fatherContactNo: { type: String },
-        motherName: { type: String },
-        motherOcupation: { type: String },
-        motherContactNo: { type: String },
-    },
-    localGuardian: {
-        name: { type: String },
-        contactNo: { type: String },
-        ocupation: { type: String },
-        adress: { type: String }
-
-    },
+    guardian: GuardianSchema,
+    localGuardian: localGuardianShcema,
     profileImage: { type: String },
     isActive: ["active", "inActive"]
 
 
 })
+
+const Student = model<Student>('Student', StudentSchema)
