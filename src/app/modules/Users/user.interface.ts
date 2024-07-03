a59029e53b29206a22392a8c982e2fa20df80aff
Siftab <siiffuuu@gmail.com>
2024-07-03 13:29:54 +0600

    user model added

--- src/app/modules/Users/user.interface.ts
+++ src/app/modules/Users/user.interface.ts
@@ -4,8 +4,8 @@ export type TUser = {
     id: string,
     password: string,
     needsPasswordChange: boolean,
-    role: "admin" | "stdudent" | "faculty ",
+    role: "admin" | "student" | "faculty ",
     status: "in-progress" | "blocked"
     isDeleted: boolean,
-    isUpadted: boolean
+
 }
---
+++ src/app/modules/Users/user.schema.ts
@@ -0,0 +1,41 @@
+import { required } from "joi";
+import { Schema, model } from "mongoose";
+import { TUser } from "./user.interface";
+
+
+
+export const userSchema = new Schema<TUser>({
+    id: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    needsPasswordChange: {
+        type: Boolean,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["admin", "student", "faculty "],
+        required: true
+    }
+    , status: {
+        type: String,
+        enum: ["in-progress", "blocked"],
+        required: true
+    },
+    isDeleted: {
+        type: Boolean,
+        required: true
+    }
+
+
+}, {
+    timestamps: true
+})
+
+
+export const User = model<TUser>("User", userSchema)
